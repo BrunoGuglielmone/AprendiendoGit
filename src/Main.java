1faf46367b744af48e1b959497a551fd59e9b9ac
Bruno Guglielmone <bguglielmone@docente.ceibal.edu.uy>
2024-09-13 22:14:04 -0300

    Se agrega mensaje por division por 0

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenido al sistema!");
-        int var = 7/2;
+        try{
+            int var = 7/0;
+        } catch (Exception e){
+            System.out.println("No se puede dividir por 0");
+        }
     }
 }
